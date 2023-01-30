de2578e9e6f7c392d20c637b7ce6621495567965
kkuismin <katariina.kuismin@gmail.com>
2023-01-30 20:19:31 +0200

    Exercise done

--- App.js
+++ App.js
@@ -1,10 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState('');
+  const [data, setData] = useState([]);
+
+  const lisaa = () => {
+    const summa = parseInt(number1) + parseInt(number2);
+    setResult(summa);
+    const history = number1 + " + " + number2 + " = " + summa;
+    setData([...data, {key: history}]);
+    setNumber1('');
+    setNumber2('');
+  }
+
+  const vahenna = () => {
+    const vahennys = parseInt(number1) - parseInt(number2);
+    setResult(vahennys);
+    const history = number1 + " - " + number2 + " = " + vahennys;
+    setData([...data, {key: history}]);
+    setNumber1('');
+    setNumber2('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Result: {result}</Text>
+      <TextInput 
+        style={styles.input}
+        onChangeText={number1 => setNumber1(number1)}
+        value={number1}
+        keyboardType="numeric"
+      />
+      <TextInput 
+        style={styles.input}
+        onChangeText={number2 => setNumber2(number2)}
+        value={number2}
+        keyboardType="numeric"
+      />
+      <View style={styles.buttons}>
+        <Button onPress={lisaa} title="+" />
+        <Button onPress={vahenna} title="-" />
+      </View>
+      <Text style={styles.text}>History</Text>
+      <FlatList 
+        style={styles.flatlist} 
+        data={data}
+        renderItem={({ item }) => <Text>{ item.key }</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -23,10 +71,17 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     margin: 5,
   },
+  text: {
+    fontSize: 16,
+    marginTop: 50,
+  },
   buttons: {
     width: 100,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     margin: 10,
   },
+  flatlist: {
+    margin: 10,
+  }
 });
